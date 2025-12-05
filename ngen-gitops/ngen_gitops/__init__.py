f0b603c46c5343e87d8fa54828d9676e13492b2f
nurahmat <mamat@qoin.id>
2025-12-05 10:41:33 +0700

    Bump version to 0.1.10

--- ngen-gitops/ngen_gitops/__init__.py
+++ ngen-gitops/ngen_gitops/__init__.py
@@ -1,5 +1,5 @@
 """ngen-gitops - GitOps CLI and web server for Bitbucket operations."""
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 __author__ = "ngen-gitops contributors"
 __description__ = "GitOps CLI and web server for Bitbucket operations"
